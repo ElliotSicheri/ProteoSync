5de2d532f3e7fd77d4e1798568206b158cbd8880
Elliot Sicheri <113069552+Spaghetti-12@users.noreply.github.com>
2025-08-07 11:12:47 -0400

    main.py now creates required directories if not present at runtime

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from Code.AlignController import AlignController
 from Code.AlignGUI import AlignGUI
+from os.path import exists
+import os
 
 if __name__ == "__main__":
+    if not exists('./downloads'):
+        os.mkdir('./downloads')
+        os.mkdir('./downloads/fastas')
+        os.mkdir('./downloads/pdb_structures')
+        os.mkdir('./downloads/AF_structures')
+
+    if not exists('./output'):
+        os.mkdir('./output')
+
+    if not exists('./temp_files'):
+        os.mkdir('./temp_files')
+
     cont = AlignController()
     gui = AlignGUI(cont)
