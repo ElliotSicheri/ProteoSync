636043bb6bd1f67c53be4faafacdddd76e81d9ac
Elliot Sicheri <113069552+Spaghetti-12@users.noreply.github.com>
2025-09-06 12:06:24 -0400

    Revert "User can now include more than 1 sequence per species in the output alignment"
    This reverts commit d2105b945fb35ac123e15094c98b81e818175bcb.

--- Code/BlastSearch.py
+++ Code/BlastSearch.py
@@ -8,8 +8,7 @@ import os
 base_path = '.'
 
 
-def blast_search(seq_file: str, i_threshold_low: int, i_threshold_high: int, len_threshold: int, path_list: list[str],
-                 num_hits: int = 1) \
+def blast_search(seq_file: str, i_threshold_low: int, i_threshold_high: int, len_threshold: int, path_list: list[str]) \
         -> (dict[str: str], dict[str: int], dict[str: (int, int)]):
     """
     BLAST searches the query sequence from seq_file against a set of BLAST-formatted databases given by path_list.
@@ -22,7 +21,6 @@ def blast_search(seq_file: str, i_threshold_low: int, i_threshold_high: int, len
         i_threshold_high: int, upper % identity threshold for which BLAST hits are included
         len_threshold: int, % length threshold with respect to the length query sequence
         path_list: list[str], list of paths which contain databases to be searched
-        num_hits: int, max number of sequences to include from each species.
 
     Returns:
         -   dict of datasets that returned a hit above the thresholds mapped to the accession code of the hit sequence
@@ -52,26 +50,18 @@ def blast_search(seq_file: str, i_threshold_low: int, i_threshold_high: int, len
                                       out=base_path+'/temp_files/raw_data.txt', outfmt=0,
                                       num_threads=8, num_alignments=5, num_descriptions=5)
         blast()
-        hits_list = _return_first_hits(num_hits)
+        sequence, code, score = _return_first_hit()
         species = os.path.basename(path)
-        i = 0
-        hits_list = sorted(hits_list, key=lambda x: x[-1], reverse=True)
-        for sequence, code, score in hits_list:
-            len_score = int((len(sequence) / query_len) * 100)
+        len_score = int((len(sequence) / query_len) * 100)
 
-            s_tag = species
-            if i > 0:
-                s_tag += f'_{i + 1}'
-
-            if score < i_threshold_low or score > i_threshold_high:
-                failed_iden[s_tag] = score
-            elif low_lim > len_score or len_score > high_lim:
-                failed_len[s_tag] = (score, len_score)
-            else:
-                formatted_results += '> ' + s_tag + '_[' + str(score) + '] \n'
-                formatted_results += sequence + '\n'
-                successful_searches[s_tag] = code
-            i += 1
+        if score < i_threshold_low or score > i_threshold_high:
+            failed_iden[species] = score
+        elif low_lim > len_score or len_score > high_lim:
+            failed_len[species] = (score, len_score)
+        else:
+            formatted_results += '> ' + species + '_[' + str(score) + '] \n'
+            formatted_results += sequence + '\n'
+            successful_searches[species] = code
 
     with open(base_path+'/temp_files/formatted_results.txt', 'w') as file:
         # Clears the contents of formatted_results.txt and writes the formatted data to the file
@@ -81,14 +71,10 @@ def blast_search(seq_file: str, i_threshold_low: int, i_threshold_high: int, len
     return successful_searches, failed_iden, failed_len
 
 
-def _return_first_hits(num_hits: int = 1) -> list[(str, str, int)]:
+def _return_first_hit() -> (str, str, int):
     """
-    Returns the sequence, accession code and % identity of the first num_hits hits in the BLAST search output file.
+    Returns the sequence, accession code and % identity of the first hit in the BLAST search output file.
     """
-
-    hit_list = []
-
-    hits = 0
 
     hit = ''
     code = ''
@@ -102,33 +88,15 @@ def _return_first_hits(num_hits: int = 1) -> list[(str, str, int)]:
 
     for line in result_lines:
         if line != '' and line[0] == '>':
-            if hits == num_hits:
-                keys = list(segment_dict.keys())
-                while len(keys) > 0:
-                    smallest = min(keys)
-                    hit += segment_dict[smallest]
-                    keys.remove(smallest)
-                hit_list.append((hit.replace('-', ''), code, score))
-                break
-
+            # If the line contains the name of a BLAST hit after the sequence has been added, stop adding sequence lines
+            # to the output string
             if started:
-                keys = list(segment_dict.keys())
-                while len(keys) > 0:
-                    smallest = min(keys)
-                    hit += segment_dict[smallest]
-                    keys.remove(smallest)
-                hit_list.append((hit.replace('-', ''), code, score))
-                hit = ''
-                code = ''
-                score = 0
-                segment_dict = {}
-
-            c_start = line.find(' ')
-            c_start = line.find(' ', c_start + 1)
-            code = line[2:c_start]
-            started = True
-
-            hits += 1
+                break
+            else:
+                c_start = line.find(' ')
+                c_start = line.find(' ', c_start + 1)
+                code = line[2:c_start]
+                started = True
         elif 'Identities' in line:
             i = line.find('%')
             score_str = line[i - 3:i].replace('(', '')
@@ -142,15 +110,12 @@ def _return_first_hits(num_hits: int = 1) -> list[(str, str, int)]:
             seq_line = line[12:end].lstrip()
             segment_dict[start_value] = seq_line
         elif 'No hits found' in line:
-            return [('', '', 0)]
+            return '', '', 0
 
-    if hits != num_hits:
-        # We've reached the end of the file. append the last hit.
-        keys = list(segment_dict.keys())
-        while len(keys) > 0:
-            smallest = min(keys)
-            hit += segment_dict[smallest]
-            keys.remove(smallest)
-        hit_list.append((hit.replace('-', ''), code, score))
+    keys = list(segment_dict.keys())
+    while len(keys) > 0:
+        smallest = min(keys)
+        hit += segment_dict[smallest]
+        keys.remove(smallest)
 
-    return hit_list
+    return hit.replace('-', ''), code, score
